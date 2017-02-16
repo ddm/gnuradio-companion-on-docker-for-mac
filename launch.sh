ead17363a103505c086dbaf20f8d104d70742e53
Dimitri del Marmol <dimitri.delmarmol@gmail.com>
2017-02-16 12:59:15 +0100

    Use dimdm/gnuradio image instead of local build

--- launch.sh
+++ launch.sh
@@ -7,7 +7,6 @@ DIR=`pwd -P`
 popd > /dev/null
 
 cd $DIR
-docker build . -t gnuradio
 
 # I have no idea what I'm doing
 rm -rf $DIR/.X11-unix
@@ -16,4 +15,4 @@ ln -s /tmp/.X11-unix/X0 $DIR/.X11-unix/X0
 ip=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
 xhost + $ip
 
-docker run -t -i -e DISPLAY=$ip:0 -v $DIR/.X11-unix:/tmp/.X11-unix -v $DIR/flows:/flows gnuradio $COMMAND
+docker run -t -i -e DISPLAY=$ip:0 -v $DIR/.X11-unix:/tmp/.X11-unix -v $DIR/flows:/flows dimdm/gnuradio $COMMAND
